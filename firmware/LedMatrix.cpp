3aba8e87d15d4c3bd16bb99d1632d241ca376f67
mars <mars>
2009-10-18 01:30:43 +0000

    added original preamble

--- firmware/LedMatrix.cpp
+++ firmware/LedMatrix.cpp
@@ -1,3 +1,23 @@
+/*  Based on code by Alex Leone. Original file header:
+
+    Copyright (c) 2009 by Alex Leone <acleone ~AT~ gmail.com>
+
+    This file is part of the Arduino TLC5940 Library.
+
+    The Arduino TLC5940 Library is free software: you can redistribute it
+    and/or modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    The Arduino TLC5940 Library is distributed in the hope that it will be
+    useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with The Arduino TLC5940 Library.  If not, see
+    <http://www.gnu.org/licenses/>. */
+
 #include "LedController.h"
 
 #include <stdlib.h>
