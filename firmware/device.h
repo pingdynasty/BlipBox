40447b675748fa8c9a8b5d78b4bdb54d00e7b66f
mars <mars>
2009-12-27 12:57:40 +0000

    removed p2 settings

--- firmware/device.h
+++ firmware/device.h
@@ -6,47 +6,7 @@
 #define BLIPBOX_V6
 /* #define BLIPBOX_P4 */
 
-#if defined BLIPBOX_P2
-
-#define TLC_SCLK_DDR   DDRB
-#define TLC_SCLK_PORT  PORTB
-#define TLC_SCLK_PIN   PB0
-
-#define TLC_XLAT_DDR   DDRB
-#define TLC_XLAT_PORT  PORTB
-#define TLC_XLAT_PIN   PB1
-
-#define TLC_BLANK_DDR  DDRB
-#define TLC_BLANK_PORT PORTB
-#define TLC_BLANK_PIN  PB2
-
-#define TLC_GSCLK_DDR  DDRB
-#define TLC_GSCLK_PORT PORTB
-#define TLC_GSCLK_PIN  PB3
-
-#define TLC_VPRG_DDR   DDRB
-#define TLC_VPRG_PORT  PORTB
-#define TLC_VPRG_PIN   PB4
-
-#define TLC_SIN_DDR    DDRB
-#define TLC_SIN_PORT   PORTB
-#define TLC_SIN_PIN    PB5
-
-#define LED_ROW_DDR    DDRD
-#define LED_ROW_PORT   PORTD
-#define LED_ROW_1      PD3
-#define LED_ROW_2      PD4
-#define LED_ROW_3      PD5
-#define LED_ROW_4      PD6
-#define LED_ROW_5      PD7
-
-#define PIN_RT 1 // right top // pin 1
-#define PIN_RL 0 // right low // pin 2
-#define PIN_SG 2 // wiper     // pin 3
-#define PIN_LT 3 // left top  // pin 4
-#define PIN_LL 4 // left low  // pin 5
-
-#elif defined BLIPBOX_P4
+#if defined BLIPBOX_P4
 
 #define TLC_SCLK_DDR   DDRB
 #define TLC_SCLK_PORT  PORTB
