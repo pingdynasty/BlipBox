9aa89bd7aa67e223e130e7d77aa63cc1674f7df7
mars <mars@pingdynasty.com>
2011-07-11 14:19:42 +0100

    set lock fuse

--- tools/burn-bootloader-ATMega168.sh
+++ tools/burn-bootloader-ATMega168.sh
@@ -4,6 +4,7 @@ PROG=avrusb500
 MCU=m168
 HEX=/Users/mars/arduino/arduino-0016/hardware/bootloaders/atmega/ATmegaBOOT_168_diecimila.hex
 check="avrdude -c $PROG -p $MCU -v -P $PORT"
-fuses="avrdude -c $PROG -p $MCU -U lfuse:w:0xff:m -U hfuse:w:0xdd:m -U efuse:w:0x00:m -P $PORT"
-burn="avrdude -c $PROG -p $MCU -U flash:w:$HEX  -P $PORT"
-$check && $fuses && $burn
+fuses="avrdude -c $PROG -p $MCU -U lock:w:0x3f:m -U lfuse:w:0xff:m -U hfuse:w:0xdd:m -U efuse:w:0x00:m -P $PORT"
+flash="avrdude -c $PROG -p $MCU -U flash:w:$HEX  -P $PORT"
+lock="avrdude -c $PROG -p $MCU -U lock:w:0x0f:m -P $PORT"
+$fuses && $flash && $lock
