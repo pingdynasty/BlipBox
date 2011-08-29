5e9c0cb6e5fb0f7ee0204388eb9d4c920be626f5
mars <mars@pingdynasty.com>
2011-08-29 22:00:50 +0100

    moved new and delete operator definitions to operators.cpp

---
+++ firmware/operators.cpp
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+
+void* operator new(size_t size) { return malloc(size); }
+void operator delete(void* ptr) { free(ptr); }
+void * operator new (size_t, void * p) { return p ; }
