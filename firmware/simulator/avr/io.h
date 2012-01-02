520814da612f03d049709be1658e274a2da4a878
mars <martin@rebel-it.com>
2012-01-02 14:01:09 +0000

    changed pin name defines to new PORT format

--- firmware/simulator/avr/io.h
+++ firmware/simulator/avr/io.h
@@ -12,9 +12,9 @@ extern "C" {
   extern uint8_t TCCR0A, WGM01, WGM00, TCCR0B, CS01, CS00, TIMSK0, TOIE0;
   extern uint8_t DDRB, DDRC, DDRD;
   extern uint8_t PORTB, PORTC, PORTD;
-  extern uint8_t PB0, PB1, PB2, PB3, PB4, PB5, PB6, PB7;
-  extern uint8_t PC0, PC1, PC2, PC3, PC4, PC5, PC6, PC7;
-  extern uint8_t PD0, PD1, PD2, PD3, PD4, PD5, PD6, PD7;
+  extern uint8_t PORTB0, PORTB1, PORTB2, PORTB3, PORTB4, PORTB5, PORTB6, PORTB7;
+  extern uint8_t PORTC0, PORTC1, PORTC2, PORTC3, PORTC4, PORTC5, PORTC6, PORTC7;
+  extern uint8_t PORTD0, PORTD1, PORTD2, PORTD3, PORTD4, PORTD5, PORTD6, PORTD7;
   extern uint8_t UDR0;
 
 #ifdef __cplusplus
