75d9e2bafab65327b9ee0fa85097e34a8d33695d
mars <martin@rebel-it.com>
2012-01-15 13:57:42 +0000

    removed commented code

--- firmware/EventHandler.cpp
+++ firmware/EventHandler.cpp
@@ -16,5 +16,15 @@ void DefaultEventHandler::handle(TouchEvent& event){
     sendPositionMessage(event.getPosition()->x, event.getPosition()->y);
     break;
   }
+//   if(event.isTouch()){
+//     if(event.isPress() || event.isDrag()){
+//       sendPositionMessage(event.getPosition()->x, event.getPosition()->y);
+//     }else if(event.isRelease()){
+//       sendReleaseMessage();
+//     }else if(event.isTapTap()){
+//       if(event.getPosition()->getColumn() == 0 && event.getPosition()->getRow() == 0)
+// 	blipbox.setMode(EDIT_MODE);
+//     }
+//   }
 }
 
