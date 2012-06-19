ad966e5f7312818d7fea72f2c76d001522d6a927
mars <martin@rebel-it.com>
2012-06-19 15:01:21 +0100

    changed serialInput()

--- firmware/PresetCommandReader.cpp
+++ firmware/PresetCommandReader.cpp
@@ -5,24 +5,18 @@
 #define MIDI_ZONE_PRESET_LENGTH 1+MIDI_ZONES_IN_PRESET*MIDI_ZONE_PRESET_SIZE+1
 
 #define NO_COMMAND -1
-PresetCommandReader::PresetCommandReader() : command(NO_COMMAND) {}
 
 void PresetCommandReader::serialInput(unsigned char c){
-  if(command == NO_COMMAND){
-    command = c & 0xf0;
-    index = c & 0x0f;
-  }else if(command == PRESET_REQUEST_COMMAND){
-    blipbox.resetSerialReader();
-    if(c == 0){
-      blipbox.sendPreset(index);
-      blipbox.message(ALERT);
-    }else{
-      blipbox.message(MESSAGE_READ_ERROR);
-    }
+  int8_t command = c & 0xf0;
+  uint8_t index = c & 0x0f;
+  if(command == PRESET_REQUEST_COMMAND){
+    blipbox.sendPreset(index);
+    blipbox.message(ALERT);
+    blipbox.resetSerialReader(); // deletes this instance
   }else if(command == PRESET_RECEIVE_COMMAND){
     blipbox.receivePreset(index);
   }else{
-    blipbox.resetSerialReader();
     blipbox.message(MESSAGE_READ_ERROR);
+    blipbox.resetSerialReader(); // deletes this instance
   }
 }
